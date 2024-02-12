f2e821ccba2229ae837249c10d47c75b76e1421f
David Drysdale <drysdale@google.com>
2024-02-12 15:49:29 +0000

    Conditionally enable KeyMint tests
    The KeyMint Rust TA may or may not be installed on a device.
    Make the port tests cope with either eventuality, and re-enabled
    some as a result.
    
    Bug: 324883625
    Test: build-root/build-qemu-generic-arm64-test-debug/run --headless --boot-test "com.android.trusty.rust.keymint.test"
    Test: (repeat with KM/Rust both enabled and disabled on qemu)
    Change-Id: I3a6369bf791a5d7f3356529619452d2ad229ea90

--- unauthorized_test_app/rules.mk
+++ unauthorized_test_app/rules.mk
@@ -30,4 +30,10 @@ MODULE_LIBRARY_DEPS += \
 
 MODULE_RUST_TESTS := true
 
+# The port tests are built and installed regardless of whether the KeyMint Rust TA
+# is enabled, so set a config value to allow tests that involve the TA to be skipped.
+ifeq (rust,$(TRUSTY_KEYMINT_IMPL))
+     MODULE_RUSTFLAGS += --cfg 'kmr_enabled'
+endif
+
 include make/library.mk
